cd96cd58ee3dcc4c57c2f553aa8e2282d044bf3e
Brahmahamadou <124585116+Brahmahamadou@users.noreply.github.com>
2024-04-19 01:17:54 +0100

    creation des fichiers css

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,7 +3,8 @@ import React from "react"
 function Footer() {
     return (
         <footer>
-        <p>&copy; 2024 for Little Lemon By MHD</p>      </footer>
+        <p>&copy; 2024 for Little Lemon By MHD</p>  
+        </footer>
     )
 }
 export default Footer
--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -1,9 +1,11 @@
 import React from "react"
+import './Nav.css'
 
 function Nav() {
     return (
         <nav>
       <ul>
+      <li><a href="/"><img src={process.env.PUBLIC_URL + '/icons_assets/Asset 16@4x.png'} alt="Home" style={{ width: '200px', height: '50px' }} /></a></li>
         <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
         <li><a href="/Menu">Menu</a></li>
@@ -11,7 +13,8 @@ function Nav() {
         <li><a href="/about">Order Onine</a></li>
         <li><a href="/Menu">Login</a></li>
       </ul>
-    </nav>
+       </nav>
     )
+
 }
 export default Nav
