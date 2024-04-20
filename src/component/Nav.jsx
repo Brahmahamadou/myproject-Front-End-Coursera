3a8febc181bc97e61785a8ce17120eabccb81ee0
Brahmahamadou <124585116+Brahmahamadou@users.noreply.github.com>
2024-04-20 21:29:55 +0100

    style pour Navbar

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -4,8 +4,8 @@ import './Nav.css'
 function Nav() {
     return (
         <nav>
+                <img src={process.env.PUBLIC_URL + '/icons_assets/Asset 16@4x.png'} alt="Home" style={{ width: '200px', height: '50px' }} />
       <ul>
-      <li><a href="/"><img src={process.env.PUBLIC_URL + '/icons_assets/Asset 16@4x.png'} alt="Home" style={{ width: '200px', height: '50px' }} /></a></li>
         <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
         <li><a href="/Menu">Menu</a></li>
